95f190a857d1020d7a18ec779d3fe814f155db0d
MartaGacek <155073431+MartaGacek@users.noreply.github.com>
2024-02-09 11:35:37 +0100

    Update class_bear.py

--- class_bear.py
+++ class_bear.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class Bear:
     def __init__(self, params):
         self.params = params
-        self.matrices = {}
-        self.matrix = np.zeros((111, 101))
+        self.matrices = {"u": {}, "v": {}}
+        self.matrix = {"u": np.zeros((111, 111)), "v": np.zeros((111, 111))}
 
     def build_partial_matrices(self):
         for key in self.params["indexes"].keys():
@@ -13,11 +14,15 @@ class Bear:
             max_row = max([tup[0] for tup in self.params["indexes"][key]])
             min_col = min([tup[1] for tup in self.params["indexes"][key]])
             max_col = max([tup[1] for tup in self.params["indexes"][key]])
+
             if key not in self.matrices.keys():
-                self.matrices[key] = np.zeros((max_row - min_row, max_col - min_col))
-                self.matrices[key] = self.params["masks"][key]
+                self.matrices["u"][key] = np.ones((max_row - min_row, max_col - min_col))
+                self.matrices["u"][key] *= self.params["masks"]["u"][key]
+                self.matrices["v"][key] = np.ones((max_row - min_row, max_col - min_col))
+                self.matrices["v"][key] *= self.params["masks"]["v"][key]
             else:
-                self.matrices[key] = self.matrix[min_row: max_row, min_col: max_col]
+                self.matrices["u"][key] = self.matrix["u"][min_row: max_row, min_col: max_col]
+                self.matrices["v"][key] = self.matrix["v"][min_row: max_row, min_col: max_col]
         return self
 
     def build_result_matrix(self):
@@ -26,75 +31,77 @@ class Bear:
             max_row = max([tup[0] for tup in self.params["indexes"][key]])
             min_col = min([tup[1] for tup in self.params["indexes"][key]])
             max_col = max([tup[1] for tup in self.params["indexes"][key]])
-            self.matrix[min_row: max_row, min_col: max_col] = self.matrices[key]
+
+            result_min_row = self.params["indexes"][key][0][0]
+            result_max_row = self.params["indexes"][key][2][0]
+            result_min_col = self.params["indexes"][key][0][1]
+            result_max_col = self.params["indexes"][key][1][1]
+
+            self.matrix["u"][result_min_row:result_max_row, result_min_col:result_max_col] = self.matrices["u"][key]
+            self.matrix["v"][result_min_row:result_max_row, result_min_col:result_max_col] = self.matrices["v"][key]
         return self
 
-
-    def solve_area(self, D_v, D_u, T, h_t, h_x, u_0, v_0, f, g):
-        for t, ind_t in enumerate(T):
-            for key in self.matrices.keys():
+    def solve_area(self, D_v, D_u, T, h_t, h_x, u_0, v_0, g, C_1, C_2, C_3, C_4, C_5):
+        for ind_t, t in enumerate(T):
+            for key in self.matrices["u"].keys():
                 min_row = min([tup[0] for tup in self.params["indexes"][key]])
                 max_row = max([tup[0] for tup in self.params["indexes"][key]])
                 min_col = min([tup[1] for tup in self.params["indexes"][key]])
                 max_col = max([tup[1] for tup in self.params["indexes"][key]])
                 x = np.arange(min_row, max_row, h_x)
                 y = np.arange(min_col, max_col, h_x)
-                #xx = np.meshgrid(x, y)[0]
-                #yy = np.meshgrid(x, y)[1]
-                final_matrix_u = np.zeros(x.shape[0], y.shape[1], len(T))
-                final_matrix_v = np.zeros(x.shape[0], y.shape[1], len(T))
+                # xx, yy = np.meshgrid(x, y)
+                xx = np.meshgrid(x, y)[0]
+                yy = np.meshgrid(x, y)[1]
+                final_matrix_u = self.matrices["u"][key]
+                final_matrix_v = self.matrices["v"][key]
                 if t == 0:
-                    final_matrix_u = u_0(x, y)
-                    final_matrix_v = v_0(x, y)
-                    #.... tu coś będzie
+                    final_matrix_u = u_0(xx, yy)
+                    final_matrix_v = v_0(xx, yy)
                 else:
-                    final_matrix_u[0, :, ind_t+1] = final_matrix_u[1,:,t+1] + h_x*g(T[t+1])
-                    final_matrix_u[-1,:,t+1] = final_matrix_u[-2,:,t+1] + h_x*g(T[t+1])
-                    final_matrix_u[:,0,t+1] = final_matrix_u[:,1,t+1] + h_x*g(T[t+1])
-                    final_matrix_u[:,-1,t+1] = final_matrix_u[:,-2,t+1] + h_x*g(T[t+1])
-                    final_matrix_u[1:-1,1:-1, t+1] = h_t * (D_u/(h_x**2) * (final_matrix_u[2:, 1:-1, t] + + final_matrix_u[:-2, 1:-1, t] 
-                                                               + final_matrix_u[1:-1, :-2, t] + final_matrix_u[1:-1, 2:, t] - 4 * final_matrix_u[1:-1, 1:-1, t]) 
-                                               - C_1*final_matrix_u[1:-1, 1:-1, t] + (C_2*final_matrix_u[1:-1, 1:-1, t]**2)/final_matrix_v[1:-1, 1:-1, t + C_3) + final_matrix_u[1:-1, 1:-1, t]
-                    final_matrix_v[0, :, t+1] = final_matrix_v[1,:,t+1] + h_x*g(T[t+1])
-                    final_matrix_v[-1,:,t+1] = final_matrix_v[-2,:,t+1] + h_x*g(T[t+1])
-                    final_matrix_v[:,0,t+1] = final_matrix_v[:,1,t+1] + h_x*g(T[t+1])
-                    final_matrix_v[:,-1,t+1] = final_matrix_v[:,-2,t+1] + h_x*g(T[t+1])
-                    final_matrix_v[1:-1,1:-1, t+1] = h_t * (D_v/(h_x**2) * (final_matrix_v[2:, 1:-1, t] + final_matrix_v[:-2, 1:-1, t] + final_matrix_v[1:-1, :-2, t] + 
-                                                             final_matrix_v[1:-1, 2:, t] - 4 * final_matrix_v[1:-1, 1:-1, t]) + C_4*final_matrix_u[1:-1, 1:-1, t]**2 - 
-                                                            C_5*final_matrix_v[1:-1, 1:-1, t]) + final_matrix_v[1:-1, 1:-1, t]
+                    self.matrices["u"][key][1:-1, 1:-1] = h_t * (D_u / (h_x ** 2) * (final_matrix_u[2:, 1:-1] +
+                                                                                     final_matrix_u[:-2,
+                                                                                     1:-1] + final_matrix_u[
+                                                                                             1:-1, :-2]
+                                                                                     + final_matrix_u[1:-1,
+                                                                                       2:] - 4 * final_matrix_u[1:-1,
+                                                                                                 1:-1]) -
+                                                                 C_1 * final_matrix_u[1:-1, 1:-1] + (
+                                                                         C_2 * final_matrix_u[1:-1, 1:-1] ** 2) /
+                                                                 final_matrix_v[1:-1, 1:-1] + C_3) + final_matrix_u[
+                                                                                                     1:-1, 1:-1]
+                    self.matrices["u"][key][0, :] = final_matrix_u[1, :] + h_x * g(T[ind_t])
+                    self.matrices["u"][key][-1, :] = final_matrix_u[-2, :] + h_x * g(T[ind_t])
+                    self.matrices["u"][key][:, 0] = final_matrix_u[:, 1] + h_x * g(T[ind_t])
+                    self.matrices["u"][key][:, -1] = final_matrix_u[:, -2] + h_x * g(T[ind_t])
+                    self.matrices["v"][key][1:-1, 1:-1] = h_t * (
+                            D_v / (h_x ** 2) * (final_matrix_v[2:, 1:-1] + final_matrix_v[:-2, 1:-1] +
+                                                final_matrix_v[1:-1, :-2] + final_matrix_v[1:-1, 2:] - 4 *
+                                                final_matrix_v[1:-1, 1:-1]) + C_4 * final_matrix_u[1:-1,
+                                                                                    1:-1] ** 2 -
+                            C_5 * final_matrix_v[1:-1, 1:-1]) + final_matrix_v[1:-1, 1:-1]
+
+                    self.matrices["v"][key][0, :] = final_matrix_v[1, :] + h_x * g(T[ind_t])
+                    self.matrices["v"][key][-1, :] = final_matrix_v[-2, :] + h_x * g(T[ind_t])
+                    self.matrices["v"][key][:, 0] = final_matrix_v[:, 1] + h_x * g(T[ind_t])
+                    self.matrices["v"][key][:, -1] = final_matrix_v[:, -2] + h_x * g(T[ind_t])
+
+                self.matrices["u"][key] = final_matrix_u
+                self.matrices["v"][key] = final_matrix_v
+
+                if self.matrix["u"][min_row:max_row, min_col:max_col].shape == self.matrices["u"][key].shape:
+                    self.matrix["u"][min_row:max_row, min_col:max_col] = self.matrices["u"][key]
+                else:
+                    pass
+
+                if self.matrix["v"][min_row:max_row, min_col:max_col].shape == self.matrices["v"][key].shape:
+                    self.matrix["v"][min_row:max_row, min_col:max_col] = self.matrices["v"][key]
+                else:
+                    pass
 
 
-
-
-
-
-
-
-
-        
-            #self.matrices[key][1:-1,1:-1] =
-                # środek
-                #self.matrices[key][1:-1, 1:-1] = self.matrices[key][1:-1, 1:-1] + ( ) * ( )  + self.params['force_term'](self.matrices[key][1:-1, 1:-1])
-                # ściany
-        # for key in self.matrices.keys():
-        #     for t in T:
-        #         if t = 0:
-        #             u[:,:,0]= u_0()
-        #             v[:,:,0]= u_0()
-        #         else:
-        #             u[1:-1,1:-1, k] = h_t * (D_u/(h_x**2) * (u[2:, 1:-1, k-1] + + u[:-2, 1:-1, k-1] + u[1:-1, :-2, k-1] + u[1:-1, 2:, k-1] - 4 * u[1:-1, 1:-1, k-1]) - C_1*u[1:-1, 1:-1, k-1] + (C_2*u[1:-1, 1:-1, k-1]**2)/v[1:-1, 1:-1, k-1 + C_3) + u[1:-1, 1:-1, k-1]
-        #             u[0,:,k] = u[1,:,k] + h_x*g(T[k])
-        #             u[-1,:,k] = u[-2,:,k] + h_x*g(T[k])
-        #             u[:,0,k] = u[:,1,k] + h_x*g(T[k])
-        #             u[:,-1,k] = u[:,-2,k] + h_x*g(T[k])
-        #             v[1:-1,1:-1, k] = h_t * (D_v/(h_x**2) * (v[2:, 1:-1, k-1] + v[:-2, 1:-1, k-1] + v[1:-1, :-2, k-1] + v[1:-1, 2:, k-1] - 4 * v[1:-1, 1:-1, k-1]) + C_4*u[1:-1, 1:-1, k-1]**2 - C_5*v[1:-1, 1:-1, k-1]) + v[1:-1, 1:-1, k-1]
-        #             v[0,:,k] = v[1,:,k] + h_x*g(T[k])
-        #             v[-1,:,k] = v[-2,:,k] + h_x*g(T[k])
-        #             v[:,0,k] = v[:,1,k] + h_x*g(T[k])
-        #             v[:,-1,k] = v[:,-2,k] + h_x*g(T[k])
-        #             k += 1
-                
-
+u_0 = lambda x, y: np.abs(np.random.random(x.shape))
+v_0 = lambda x, y: np.abs(np.random.random(x.shape))
 
 params = {"indexes": {"i": [(0, 0), (0, 10), (110, 0), (110, 10)],
                       "ii": [(0, 10), (0, 30), (40, 10), (40, 30)],
@@ -114,18 +121,29 @@ params = {"indexes": {"i": [(0, 0), (0, 10), (110, 0), (110, 10)],
                       "xvi": [(90, 10), (90, 50), (110, 10), (110, 50)],
                       "xvii": [(90, 50), (90, 60), (110, 50), (110, 60)],
                       "xviii": [(90, 60), (90, 100), (110, 60), (110, 100)]},
-          "masks": {"i": 0, "ii": 0, "iii": 1, "iv": 0, "v": 0, "vi": 1, "vii": 1, "viii": 0, "ix": 0,
-                    "x": 0, "xi": 1, "xii": 1, "xiii": 1, "xiv": 0, "xv": 0, "xvi": 1, "xvii": 0,
-                    "xviii": 1}}
+          "masks": {"u": {"i": 0, "ii": 0, "iii": 1, "iv": 0, "v": 0, "vi": 1, "vii": 1, "viii": 0,
+                          "ix": 0, "x": 0, "xi": 1, "xii": 1, "xiii": 1, "xiv": 0, "xv": 0, "xvi": 1, "xvii": 0,
+                          "xviii": 1}, "v": {"i": 0, "ii": 0, "iii": 1, "iv": 0, "v": 0, "vi": 1, "vii": 1, "viii": 0,
+                                             "ix": 0, "x": 0, "xi": 1, "xii": 1, "xiii": 1, "xiv": 0, "xv": 0, "xvi": 1,
+                                             "xvii": 0, "xviii": 1}}}
 
 h_t = 0.1
-u_0 = lambda x, y: np.abs(np.random.random(x.shape))
-v_0 = lambda x, y: np.abs(np.random.random(x.shape))
-T = np.arange(0, 10, h_t)
-g = lambda x, y: 0
+T = np.arange(0, 5 + h_t, h_t)
+g = lambda x: 0
+Du = 0.4
+Dv = 0.6
+h_x = 1
+C1 = 0.5
+C2 = 0.7
+C3 = 0.4
+C4 = 0.6
+C5 = 0.3
 
 coralgol = Bear(params)
 coralgol.build_partial_matrices()
 coralgol.build_result_matrix()
-plt.imshow(coralgol.matrix)
+coralgol.solve_area(Dv, Du, T, h_t, h_x, u_0, v_0, g, C1, C2, C3, C4, C5)
+plt.imshow(coralgol.matrix["u"], cmap="YlOrBr")
+plt.show()
+plt.imshow(coralgol.matrix["v"], cmap="YlOrBr")
 plt.show()
